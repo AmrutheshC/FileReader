91222be08fdf0e765f56dac5813493790e05e7da
AmrutheshC <Amruthesh.C@dionatec.com>
2015-02-03 12:06:03 +0530

    Update FileReader.js

--- www/FileReader.js
+++ www/FileReader.js
@@ -1,4 +1,4 @@
-var fileReader = {
+var mediaFileReader = {
     encryptFile: function(location, successCallback, errorCallback) {
         cordova.exec(
             successCallback, // success callback function
@@ -6,25 +6,10 @@ var fileReader = {
             'CipherUtil', // mapped to our native Java class called "Calendar"
             'encryptFile', // with this action name
             [{ // and this array of custom arguments to create our entry
-                "location": "location"
-            }]
-        );
-    },
-    decryptFile: function(location, successCallback, errorCallback) {
-        cordova.exec(
-            successCallback, // success callback function
-            errorCallback, // error callback function
-            'Calendar', // mapped to our native Java class called "Calendar"
-            'addCalendarEntry', // with this action name
-            [{ // and this array of custom arguments to create our entry
-                "title": title,
-                "description": notes,
-                "eventLocation": location,
-                "startTimeMillis": startDate.getTime(),
-                "endTimeMillis": endDate.getTime()
+                "location": location
             }]
         );
     }
 };
 
-module.exports = fileReader;
+module.exports = mediaFileReader;
