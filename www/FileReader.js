12da2427af90dcbc227af90679babdace3f3d65c
AmrutheshC <Amruthesh.C@dionatec.com>
2015-02-05 16:40:26 +0530

    Update FileReader.js

--- www/FileReader.js
+++ www/FileReader.js
@@ -9,6 +9,18 @@ var mediaFileReader = {
                 "location": location
             }]
         );
+    },
+
+    decryptFile: function(location, successCallback, errorCallback) {
+        cordova.exec(
+            successCallback, // success callback function
+            errorCallback, // error callback function
+            'CipherUtil', // mapped to our native Java class called "Calendar"
+            'decryptFile', // with this action name
+            [{ // and this array of custom arguments to create our entry
+                "location": location
+            }]
+        );
     }
 };
 
